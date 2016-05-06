e9850f46f2e685c1efe9e8cd042bfb5ead6b89e7
Simon Schuh <simon.schuh@shopgate.com>
2016-05-06 17:50:22 +0200

    Use url of current tab

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.create({'url': chrome.extension.getURL('index.html')}, function (tab) {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+      chrome.tabs.create({'url': chrome.extension.getURL('index.html') + '?url=' + url}, function (tab) {
+    });
   });
 });
