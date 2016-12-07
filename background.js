6aba55677a465d48f1239f8da1092bf9e1823eed
Gabriel Delépine <gabriel.delepine@gmail.com>
2016-12-07 09:10:04 +0100

    Autodetect raw url of .yaml files on github.com
    Use the url of the raw version of the .yaml file on github.com
    Fallback on currentUrl if detection fails

--- background.js
+++ background.js
@@ -1,7 +1,17 @@
+function openTab (url) {
+  chrome.tabs.create({'url': chrome.extension.getURL('index.html') + '?url=' + url});
+}
+
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-      chrome.tabs.create({'url': chrome.extension.getURL('index.html') + '?url=' + url}, function (tab) {
-    });
+    var currentUrl = tabs[0].url;
+
+    if ((/^https:\/\/github.com\/.+\.yaml$/).test(currentUrl)) { // yaml file in github.com interface
+      chrome.tabs.executeScript(tabs[0].id, { code: 'document.querySelector("#raw-url") ? document.querySelector("#raw-url").href : null' }, function (rawUrl) {
+        openTab(rawUrl[0] || currentUrl)
+      });
+    } else {
+      openTab(currentUrl);
+    }
   });
 });
