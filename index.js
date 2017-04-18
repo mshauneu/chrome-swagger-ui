a439894811d35d297a0fd6911cb1f14d357d2187
Mikhail Shauneu <m.shauneu@gmail.com>
2017-04-18 16:45:41 +0300

    Make  it  workable

--- index.js
+++ index.js
@@ -1,53 +1,17 @@
-  $(function () {
-   var url = window.location.search.match(/url=([^&]+)/);
-   if (url && url.length > 1) {
-     url = decodeURIComponent(url[1]);
-   } else {
-     url = "http://petstore.swagger.io/v2/swagger.json";
-   }
+window.onload = function() {
+  // Build a system
+  const ui = SwaggerUIBundle({
+    url: "http://petstore.swagger.io/v2/swagger.json",
+    dom_id: '#swagger-ui',
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  })
 
-   hljs.configure({
-     highlightSizeThreshold: 5000
-   });
-
-   // Pre load translate...
-   if(window.SwaggerTranslator) {
-     window.SwaggerTranslator.translate();
-   }
-   window.swaggerUi = new SwaggerUi({
-     url: url,
-     dom_id: "swagger-ui-container",
-     supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
-     onComplete: function(swaggerApi, swaggerUi){
-       if(typeof initOAuth == "function") {
-         initOAuth({
-           clientId: "your-client-id",
-           clientSecret: "your-client-secret-if-required",
-           realm: "your-realms",
-           appName: "your-app-name",
-           scopeSeparator: " ",
-           additionalQueryStringParams: {}
-         });
-       }
-
-       if(window.SwaggerTranslator) {
-         window.SwaggerTranslator.translate();
-       }
-     },
-     onFailure: function(data) {
-       log("Unable to Load SwaggerUI");
-     },
-     docExpansion: "none",
-     jsonEditor: false,
-     defaultModelRendering: 'schema',
-     showRequestHeaders: false
-   });
-
-   window.swaggerUi.load();
-
-   function log() {
-     if ('console' in window) {
-       console.log.apply(console, arguments);
-     }
-   }
- });
+  window.ui = ui
+}
